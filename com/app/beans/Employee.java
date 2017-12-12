635a8dd583642677d77610faa0ccf7733eda4cd3
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-12 23:32:57 +0530

    Create Employee.java

---
+++ com/app/beans/Employee.java
@@ -0,0 +1,22 @@
+package com.app.beans;
+
+public class Employee {
+
+	private int id;
+	private String name;
+	
+	public Employee(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+	
+	
+}
